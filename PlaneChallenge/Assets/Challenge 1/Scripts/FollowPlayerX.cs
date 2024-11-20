25a388fa9a4120f20731df49211d27262af9856a
Dartpopper1 <espinozaj@roverkids.org>
2024-11-20 14:14:41 -0500

    Finished Challenge 1
    + bonus

--- PlaneChallenge/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ PlaneChallenge/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new(-30, 3, -2);
 
     // Start is called before the first frame update
     void Start()
